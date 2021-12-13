8535683aada963b96c185ddd8f926ec44da7cde6
Pauline C <47113664+NeonVhenan@users.noreply.github.com>
2021-12-13 16:24:17 +0100

    BLBL

--- Assets/Scripts/Chambouletout.cs
+++ Assets/Scripts/Chambouletout.cs
@@ -4,21 +4,78 @@ using UnityEngine;
 
 public class Chambouletout : MonoBehaviour
 {
+    static List<Chambouletout> cannettes = new List<Chambouletout>();
+    static List<int> chute = new List<int>();
+    float yBase, xBase, zBase;
+    public static bool finDePartie = false;
+    public AudioSource _gameover;
+    static int cp = 0;
+    static int nb;
+    static bool flag = false;
+    int num;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cannettes.Add(GetComponent<Chambouletout>());
+
+        yBase = GetComponent<Chambouletout>().transform.position.y;
+        xBase = GetComponent<Chambouletout>().transform.position.x;
+        zBase = GetComponent<Chambouletout>().transform.position.z;
+        chute.Add(0);
+        num = cannettes.Count - 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        nb = 0;
+            Debug.Log("Base : ");
+            Debug.Log(yBase);
+            Debug.Log("valeur : ");
+            Debug.Log(GetComponent<Chambouletout>().transform.position.y - 0.1);
+            if (yBase < GetComponent<Chambouletout>().transform.position.y - 0.1)
+            {
+
+                chute[num] = 1; ;
+            }
+
+        for (int i = 0; i < cannettes.Count; i++)
+        {
+            if (chute[i] == 1)
+                nb++;
+        }
+
+        Debug.Log("NB :");
+        Debug.Log(nb);
+
+        if (nb == cannettes.Count)
+        {
+            Debug.Log(cp);
+            if (cp == 0)
+            {
+                _gameover.Play();
+                cp++;
+            }
+            else
+            {
+                if (cp == 1000)
+                {
+                    cp = 0;
+                    remiseAZero();
+                }
+                else
+                    cp++;
+            }
+        }
     }
 
 
     public static void remiseAZero()
     {
-
+        for(int i = 0; i < cannettes.Count; i++) {
+            
+            cannettes[i].transform.position = new Vector3(cannettes[i].xBase, cannettes[i].yBase, cannettes[i].zBase);
+        }
     }
 }
--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -7,10 +7,12 @@ public class Music : MonoBehaviour
     public AudioSource _background;
     public AudioSource _search;
     public AudioSource _victory;
+    public AudioSource _gameover;
 
     bool background_music = true;
     bool search_music = false;
     bool victory_music = false;
+    bool gameover_music = false;
 
     public void BackgroundMusic()
     {
@@ -18,6 +20,7 @@ public class Music : MonoBehaviour
         background_music = true;
         search_music = false;
         victory_music = false;
+        gameover_music = false;
 
         _background.Play();
     }
@@ -41,6 +44,15 @@ public class Music : MonoBehaviour
 
         {
             _victory.Stop();
+        }
+
+        if (_gameover.isPlaying)
+        {
+            gameover_music = false;
+        }
+
+        {
+            _gameover.Stop();
         }
 
         if (!_search.isPlaying && search_music == false)
@@ -72,11 +84,58 @@ public class Music : MonoBehaviour
             _search.Stop();
         }
 
+        if (_gameover.isPlaying)
+        {
+            gameover_music = false;
+        }
+
+        {
+            _gameover.Stop();
+        }
+
         if (!_victory.isPlaying && victory_music == false)
         {
 
             _victory.Play();
             victory_music = true;
+        }
+    }
+
+    public void GameoverMusic()
+    {
+
+        if (_background.isPlaying)
+        {
+            background_music = false;
+        }
+
+        {
+            _background.Stop();
+        }
+
+        if (_search.isPlaying)
+        {
+            search_music = false;
+        }
+
+        {
+            _search.Stop();
+        }
+
+        if (_victory.isPlaying)
+        {
+            victory_music = false;
+        }
+
+        {
+            _victory.Stop();
+        }
+
+        if (!_gameover.isPlaying && gameover_music == false)
+        {
+
+            _gameover.Play();
+            gameover_music = true;
         }
     }
 }

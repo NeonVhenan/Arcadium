02a32a2af47d59c7dc096c498a5734a968441e83
Pauline C <47113664+NeonVhenan@users.noreply.github.com>
2022-01-08 13:27:25 +0100

    last update

--- Assets/Scripts/Chambouletout.cs
+++ Assets/Scripts/Chambouletout.cs
@@ -10,7 +10,7 @@ public class Chambouletout : MonoBehaviour
     public static bool finDePartie = false;
     public AudioSource _gameover;
     static int cp = 0;
-    static int nb;
+    private int nb;
     static bool flag = false;
     int num;
 
@@ -24,64 +24,71 @@ public class Chambouletout : MonoBehaviour
         zBase = GetComponent<Chambouletout>().transform.position.z;
         chute.Add(0);
         num = cannettes.Count - 1;
+		if(cannettes.Count == 6)
+			flag = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        nb = 0;
-            Debug.Log("Base : ");
-            Debug.Log(yBase);
-            Debug.Log("valeur : ");
-            Debug.Log(GetComponent<Chambouletout>().transform.position.y - 0.1);
+		if(flag){
+			nb = 0;
 
-            if (yBase < GetComponent<Chambouletout>().transform.position.y - 0.1)
-            {
+			if (yBase <= GetComponent<Chambouletout>().transform.position.y - 0.03 && chute[num] == 0)
+			{
+				chute[num] = 1;;
+			}
+			
+			Debug.Log(num);
+			Debug.Log(chute[num]);
 
-                chute[num] = 1;
-            }
+			for (int i = 0; i < cannettes.Count; i++)
+			{
+				if (chute[i] == 1)
+					nb++;
+			}
 
-        for (int i = 0; i < cannettes.Count; i++)
-        {
-            if (chute[i] == 1)
-                nb++;
-        }
+			//Debug.Log("NB :");
+			//Debug.Log(nb);
 
-        Debug.Log("NB :");
-        Debug.Log(nb);
-
-        if (nb == cannettes.Count)
-        {
-            Debug.Log(cp);
-            if (cp == 0)
-            {
-                _gameover.Play();
-                cp++;
-            }
-            else
-            {
-                if (cp == 1000)
-                {
-                    cp = 0;
-                    remiseAZero();
-                }
-                else
-                    cp++;
-            }
-        }
+			if (nb == cannettes.Count)
+			{
+				Debug.Log(cp);
+				if (cp == 0)
+				{
+					_gameover.Play();
+					cp++;
+				}
+				else
+				{
+					if (cp == 500)
+					{
+						cp = 0;
+						remiseAZero();
+					}
+					else
+						cp++;
+				}
+			}
+		}
     }
 
     public void rotation(double angle)
     {
-        GetComponent<Chambouletout>().transform.rotation = new Quaternion((float)angle, (float)0.0, (float)0.0, (float)1.0);
+        GetComponent<Chambouletout>().transform.rotation = new Quaternion((float)angle, (float)90.0, (float)0.0, (float)1.0);
     }
 
     public static void remiseAZero()
     {
+		flag = false;
         for(int i = 0; i < cannettes.Count; i++) {
             
             cannettes[i].transform.position = new Vector3(cannettes[i].xBase, cannettes[i].yBase, cannettes[i].zBase);
-            cannettes[i].rotation(-90.0); //= new Quaternion(-90.0, cannettes[i].xBase, cannettes[i].yBase, cannettes[i].zBase);
+            cannettes[i].transform.rotation = new Quaternion((float)0.0, (float)-90.0, (float)-90.0, (float)1.0);
         }
+		for(int i = 0; i < cannettes.Count; i++) {
+			chute[i] = 0;
+		}
+		flag = true;
     }
 }
